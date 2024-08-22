90e64b9361be1507fef56b5c8485cf18afb3112f
zephyr-cmd <ts766878@gmail.com>
2024-08-22 23:42:36 +0530

    card slider ui-fix

--- src/app/_components/cardSlider.js
+++ src/app/_components/cardSlider.js
@@ -50,7 +50,7 @@ export default function AutoScrollSlider({ dataPoints }) {
         {dataPoints.map((card) => (
           <div
             key={card.id}
-            className="p-4 min-h-[500px] flex-shrink-0 card w-full sm:w-1/2 md:w-1/3"
+            className="p-2 pb-6 sm:p-6 min-h-[500px] flex-shrink-0 card w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
             id={`vbp-${card.id}`}
           >
             <div className="bg-white h-full shadow-md p-6 rounded-md">
