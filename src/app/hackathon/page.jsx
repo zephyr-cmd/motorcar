69949af863099261f58735d78355589ad5d61738
zephyr-cmd <ts766878@gmail.com>
2025-01-18 23:55:23 +0530

    m-fix

--- src/app/hackathon/page.jsx
+++ src/app/hackathon/page.jsx
@@ -29,11 +29,11 @@ export default function HackathonLandingPage() {
               Web Innovators Hackathon&nbsp;{" "}
               <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
                 <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-                  <span>2024</span>
+                  <span>2025</span>
                 </div>
-                <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-                  <span>2024</span>
-                </div>
+                {/* <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+                  <span>2025</span>
+                </div> */}
               </div>
             </h2>
             <section className="container mx-auto px-4 py-10 text-center">
